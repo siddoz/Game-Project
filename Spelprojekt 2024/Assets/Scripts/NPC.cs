96037cc36679ec80d6e5ac515be288f4f2c002d8
Casmera <casmera@gmail.com>
2024-01-17 09:02:55 +0100

    NPC

---
+++ Spelprojekt 2024/Assets/Scripts/NPC.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject dialogPanel;
+    public Text dialogText;
+    public string[] dialog;
+    private int index;
+    public float wordSpeed;
+    public bool playerIsClose;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && playerIsClose)
+        {
+            if (dialogPanel.activeInHierarchy)
+            {
+                zeroText();
+            }
+            else
+            {
+                dialogText.enabled = true;
+                StartCoroutine(Typing());
+            }
+        }
+    }
+    public void zeroText()
+    {
+        dialogText.text = "";
+        index = 0;
+        dialogPanel.SetActive(false);
+    }
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialog[index].ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+    public void NextLine()
+    {
+        if (index < dialog.Length - 1)
+        {
+            index++;
+            dialogText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            zeroText();
+        }
+    }
+}
