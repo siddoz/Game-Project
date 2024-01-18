98b931d9a39e93fa7059a7e649905ef26c966469
siddoz <122971976+siddoz@users.noreply.github.com>
2024-01-18 09:55:26 +0100

    ny

---
+++ Spelprojekt 2024/Assets/Scripts/EnemyHealthManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyHealthManager : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth;
+
+    public void HurtEnemy(int damageToGive)
+    {
+        currentHealth -= damageToGive;
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
